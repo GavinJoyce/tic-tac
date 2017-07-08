219d11064d58dadf19a7e900c43af08972a15e51
Gavin Joyce <gavinjoyce@gmail.com>
2017-07-08 21:15:12 +0100

    simple css and html

--- src/ui/components/tick-tac/component.ts
+++ src/ui/components/tick-tac/component.ts
@@ -1,5 +1,23 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
+
+class Board {
+  cells;
+
+  constructor() {
+    this.cells = [
+      'X', 'O', undefined,
+      undefined, undefined, undefined,
+      undefined, undefined, undefined
+    ];
+  }
+}
 
 export default class TickTac extends Component {
+  @tracked board;
 
+  constructor(options) {
+    super(options);
+
+    this.board = new Board();
+  }
 }
