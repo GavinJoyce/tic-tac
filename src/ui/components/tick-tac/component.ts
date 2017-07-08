7e333580a4a47b6e92a85aa3feb44ccbefc4027c
Gavin Joyce <gavinjoyce@gmail.com>
2017-07-08 22:13:40 +0100

    winning lines 1

--- src/ui/components/tick-tac/component.ts
+++ src/ui/components/tick-tac/component.ts
@@ -1,5 +1,18 @@
 import Component, { tracked } from '@glimmer/component';
 
+const PLAYER_X = 'X';
+const PLAYER_O = 'O';
+const WIN_LINES = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 class Board {
   cellValues;
 
@@ -9,6 +22,24 @@ class Board {
       undefined, undefined, undefined,
       undefined, undefined, undefined
     ];
+  }
+
+  get winningLine() {
+    return WIN_LINES.find(line => {
+      let first = this.cellValues[line[0]];
+      let second = this.cellValues[line[1]];
+      let third = this.cellValues[line[2]];
+      return first !== undefined && first === second &&
+             second === third;
+    });
+  }
+
+  get hasWin() {
+    return this.winningLine !== undefined;
+  }
+
+  isCellIndexWinning(index) {
+    return this.winningLine.indexOf(index) > -1;
   }
 }
 
@@ -22,7 +53,7 @@ export default class TickTac extends Component {
   }
 
   chooseCell(index) {
-    this.board.cellValues[index] = 'X';
+    this.board.cellValues[index] = PLAYER_X;
 
     this.board = this.board;
   }
