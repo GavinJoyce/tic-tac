9ca559174ea4a6741bda7d3de9e7ef99ed5025c9
Gavin Joyce <gavinjoyce@gmail.com>
2017-07-15 21:44:20 +0100

    only allow clicks on available cells

--- src/ui/components/game-cell/component.ts
+++ src/ui/components/game-cell/component.ts
@@ -4,4 +4,14 @@ export default class GameCell extends Component {
   @tracked('args') get isWinning() {
     return this.args.board.isCellIndexWinning(this.args.index);
   }
+
+  @tracked('args') get isAvailable() {
+    return this.args.board.cellValues[this.args.index] === undefined;
+  }
+
+  onClick() {
+    if(this.isAvailable) {
+      this.args.onClick(this.args.index);
+    }
+  }
 }
